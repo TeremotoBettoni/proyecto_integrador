2a23a57a9de018aff779167bbbd7bf77731087c7
littlesonik <sonik.zapata@gmail.com>
2022-12-13 00:09:38 -0300

    conectando categorias desde base de datos a la vista

--- springboot/src/main/java/cl/yose/web/services/ColegaServices.java
+++
@@ -1,8 +0,0 @@
-package cl.yose.web.services;
-
-import cl.yose.web.models.Colega;
-
-public interface ColegaServices {
-	public Colega guardarColegas(Colega colegas);
-
-}
---
+++ yose2/src/main/java/cl/yose/web/services/UsuarioServices.java
@@ -0,0 +1,19 @@
+package cl.yose.web.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import cl.yose.web.models.Usuario;
+
+public interface UsuarioServices {
+		
+		public Optional<Usuario> getUsuarioById(Long id);
+		//public Usuario guardarUsuario(Usuario usuario);
+		public String eliminarUsuario(Long id);
+		public String actializarUsuario(Usuario usuario);
+		public Usuario obtenerDatosUsuario(Long id);
+		public List<Usuario> listaUsuarios();
+		public Boolean guardarUsuario(Usuario usuario);
+		
+		public Boolean ingresoUsuario(String email, String contraseña);
+}
